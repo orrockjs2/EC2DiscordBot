305034d5102b3cae71a910d0bd75dfe804c7f229
Sc00t <37385981+orrockjs2@users.noreply.github.com>
2023-10-19 16:23:40 -0400

    Update mcbackup.sh

--- scripts/mcbackup.sh
+++ scripts/mcbackup.sh
@@ -17,8 +17,8 @@ archive_file="$day-$backup_files-.tar.gz"
 cd /opt && tar -zcvf $temp_dest$archive_file $backup_files
 
 echo "created tar in: $temp_dest$archive_file"
-echo "running: sshpass -p '3p!cCr1nge' scp -P 6690 $temp_dest$archive_file SSH_USER@138.88.142.55:$dest"
+echo "running: sshpass -p '[PASS]' scp -P 6690 $temp_dest$archive_file SSH_USER@[HOST]:$dest"
 
-sshpass -p '3p!cCr1nge' scp -P 6690 $temp_dest$archive_file SSH_USER@138.88.142.55:$dest
+sshpass -p '[PASS]' scp -P 6690 $temp_dest$archive_file SSH_USER@[HOST]:$dest
 
 rm $temp_dest$archive_file
